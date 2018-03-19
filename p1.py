44ec94166caff82a7126b4dd1102f3287d48f538
Omar L <omar.laymoun@gmail.com>
2018-03-19 18:21:34 -0500

    Final revision
    Final revision for possibly improved performance

--- p1.py
+++ p1.py
@@ -1,35 +1,23 @@
 import pandas as pd
 pd.set_option('precision', 5)
 
-df= pd.read_csv('/Users/unknown1/Desktop/Gyro3.csv')
+df= pd.read_csv('/Users/Robo2/Downloads/DataCamp - Omar/Gyro2.csv', chunksize=6)
 
 final_df = pd.DataFrame()
-i = 0
-end = len(df)
 
-while i < end:
+# each chunk is a (6,3) dataframe
+for chunk in df:
     
-    first_six = []
+    first_six = pd.Series()
     # loop through all three columns of subset of six rows
     for x in range(3):
-        first_six += ( df.iloc[i:i+6, x].values.tolist())
-    # append each dataframe of six rows to the final dataframe
-    final_df = final_df.append(pd.DataFrame(first_six).transpose())
-    i += 6
+        first_six = first_six.append(chunk.iloc[:, x], ignore_index=True)
+    
+    # convert appended series to dataframe and transpose into a dataframe row
+    six_df = pd.DataFrame(first_six)
+    six_row = six_df.transpose()
 
-# output to csv file
-final_df.to_csv('out.csv',float_format='%.5f', header = False, index = False)
-
-
-
-
-
-
-
-
-#
-#first_six =[]
-#for x in range(3):
-#    first_six += ( df.iloc[0:6, x].values.tolist())
-#
-#print(pd.DataFrame(first_six).transpose())
+    # append each new row to the final dataframe
+    final_df = final_df.append(six_row)
+    
+final_df.to_csv('new_out.csv',float_format='%.5f', header = False, index = False)
