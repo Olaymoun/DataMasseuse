de77c21fbadee34f2dc80a7d9564b8c6acbc5157
Omar L <omar.laymoun@gmail.com>
2018-03-19 17:58:15 -0500

    First working copy
    first revision of script that works

--- p1.py
+++ p1.py
@@ -3,10 +3,33 @@ pd.set_option('precision', 5)
 
 df= pd.read_csv('/Users/unknown1/Desktop/Gyro3.csv')
 
-first_six =[]
-for x in range(3):
-    first_six += ( df.iloc[0:6, x].values.tolist())
+final_df = pd.DataFrame()
+i = 0
+end = len(df)
 
-final_df = pd.DataFrame(first_six).transpose()
+while i < end:
+    
+    first_six = []
+    # loop through all three columns of subset of six rows
+    for x in range(3):
+        first_six += ( df.iloc[i:i+6, x].values.tolist())
+    # append each dataframe of six rows to the final dataframe
+    final_df = final_df.append(pd.DataFrame(first_six).transpose())
+    i += 6
 
-final_df.to_csv('firstSix.csv',float_format='%.5f', header = False, index = False)
+# output to csv file
+final_df.to_csv('out.csv',float_format='%.5f', header = False, index = False)
+
+
+
+
+
+
+
+
+#
+#first_six =[]
+#for x in range(3):
+#    first_six += ( df.iloc[0:6, x].values.tolist())
+#
+#print(pd.DataFrame(first_six).transpose())
